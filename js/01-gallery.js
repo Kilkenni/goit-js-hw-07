1fad78118ea9450744a5691ee653139e9532d991
Kilkenni <hpittman89@outlook.com>
2021-11-14 14:24:55 +0200

    part 1, task 1..2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let galleryMarkup = "";
+
+galleryItems.forEach((galleryItem) => {
+    //do magic
+    const itemMarkup = `<div class="gallery__item">
+        <a class="gallery__link" href=${galleryItem.original}>
+            <img
+            class="gallery__image"
+            src=${galleryItem.preview}
+            data-source=${galleryItem.original}
+            alt=${galleryItem.description}
+            />
+        </a>
+    </div>`;
+
+    galleryMarkup += itemMarkup;
+});
+
+const galleryElem = document.querySelector(".gallery");
+
+galleryElem.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function zoomImage(event) {
+    event.preventDefault();
+
+    const picOrigin = event.target.getAttribute("data-source");
+}
+
+galleryElem.addEventListener("click", zoomImage);
+
+//console.log(galleryItems);
